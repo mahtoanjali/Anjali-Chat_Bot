56bfb85d3504c27d7998a59e48d9970e5cc289f6
SOUL X SYSTEM <154720925+mahtoanjali@users.noreply.github.com>
2024-01-05 12:13:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 TOKEN = os.environ.get("TOKEN", "6416853198:AAH8WJ68FyVFTl5kbD5-bXbivZo94XCAUUU")
 OWNER = os.environ.get("OWNER", "QUEENx_GOD")
-GROUP = os.environ.get("GROUP", "AnjalixSupportxGroup)
+GROUP = os.environ.get("GROUP", "AnjalixSupportxGroup")
 CHANNEL = os.environ.get("CHANNEL", "MAHTOxOFFICIAL")
 BOT_NAME = os.environ.get("BOT_NAME", "ANJALI CHAT BOT")
